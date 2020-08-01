2b20b14691c74cdd5450152db06e94edfee5691b
pasindu07 <dmpasindu07@gmail.com>
2020-08-01 17:28:46 +0530

    intial

---
+++ Final/Q3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//TODO:
+
+int main(){
+
+    vector<int> weights;
+    int number_of_products,tmp_weight,number_of_containers=0,tmp_val=0;
+    cin >> number_of_products;
+
+    for(int i=0;i<number_of_products;i++){
+        cin >> tmp_weight;  
+        weights.push_back(tmp_weight);
+    }
+    
+    sort(weights.begin(),weights.end());
+
+    for(int i=0;i<number_of_products;i++){
+
+        if(weights[i]>tmp_val){
+            tmp_val = weights[i]+4;
+            number_of_containers++;
+        }
+        else
+            continue;
+    }
+
+    cout << number_of_containers <<  endl;
+
+
+    return 0;
+}
